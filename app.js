8babee5c4986e7c55f4405427f9ba5d33b8bc553
kelvin <kelvin@program.com.tw>
2019-05-31 18:03:56 +0800

    using MongoDB

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const graphqlHttp = require('express-graphql')
 const { buildSchema }= require('graphql') // buildSchema, converting string schema to JS objects  
+const mongoose = require('mongoose')
 
+const EventModel = require('./models/event.model') 
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-//  temp resolution , it will replace by using database later 
-const events = [];
 
 app.use('/graphql',graphqlHttp({
 
@@ -26,7 +25,8 @@ app.use('/graphql',graphqlHttp({
     input EventInput {
       title: String!
       description: String!
-      price: Float
+      price: Float!
+      date: String!
     }
 
     type RootQuery {
@@ -45,28 +45,53 @@ app.use('/graphql',graphqlHttp({
   // Resolver , resolver for your commands need to have the exact same name , like events or createEvent
   rootValue: {
     events: () => {
-      return events
+      return EventModel.find()
+      .then(events => {
+        return events.map(event => {
+          // we can use event.id because of mongoose will actually access the native ID and translate it into string
+          return {...event._doc, _id: event.id} 
+        })
+      })
+      .catch(err=>{
+        throw err
+      })
     },
     createEvent: (args) => {
-      const {title , description, price} = args.eventInput
+      const {title , description, price, date} = args.eventInput
 
-      const event = {
-        _id: Math.random().toString(),
+      const event = new EventModel({
         title,
         description,
         price: +price,
-        date: new Date().toISOString()
-      } 
-      events.push(event)
-      return event
+        date: new Date(date)
+      })
+
+      return event.save()
+      .then(result =>{
+        console.log(result)
+        return {...result._doc, _id: event._doc._id.toString()}
+      })
+      .catch(err => {
+        console.log(err)
+        throw err
+      })
+
     }
   },
   // built-in debugging and development tool 
   graphiql: true
 }))
 
-const port =  process.env.PORT || 4000 
-app.listen(port, ()=>{
-  console.log(`listening on port: ${port}`);
-})
+mongoose.set('useNewUrlParser', true);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-qdkg1.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+  .then(()=>{
+    console.log('mongoDB connect successful~~');
+    const port =  process.env.PORT || 4000 
+    app.listen(port, ()=>{
+      console.log(`listening on port: ${port}`);
+    })  
+  }).catch(err => {
+    console.log(err);
+  })
 
